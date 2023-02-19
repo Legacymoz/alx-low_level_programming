93cb2a05a1129cf22b5220e39068e6b08cd17a13
Legacymoz <mozartkandie@gmail.com>
2023-02-19 06:49:34 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Printing alphabet letters
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+		while (alpha <= 'z')
+		{
+			putchar (alpha);
+			alpha++;
+		}
+
+	return (0);
+}
