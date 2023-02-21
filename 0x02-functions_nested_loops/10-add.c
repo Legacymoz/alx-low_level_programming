76a0e26f1bfdfc848051115ba2b5ae853cb66b6c
Legacymoz <mozartkandie@gmail.com>
2023-02-21 12:11:38 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adding of 2 integers
+ * @n1: first number
+ * @n2: second number
+ * Return: interger value
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
