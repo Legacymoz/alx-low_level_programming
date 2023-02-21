e285018faefa42a83c49360eb26d3d6ca1c62c36
Legacymoz <mozartkandie@gmail.com>
2023-02-21 06:37:51 -0800

    task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void);
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+	}
+	_putchar(alpha);
+}
