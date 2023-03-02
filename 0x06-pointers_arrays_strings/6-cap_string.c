52c388269267d4b89f99c56efdd978aaf3791a18
Legacymoz <mozartkandie@gmail.com>
2023-03-02 13:55:53 -0800

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * cap_string - capitalizes all words
+ * @n: pointer
+ * Return: n
+ */
+char *cap_string(char *n)
+{
+
+	int index = 0;
+
+	while (n[index])
+	{
+		while (!(n[index] >= 'a' && n[index] <= 'z'))
+
+			index++;
+
+		if (n[index - 1] == ' ' ||
+		n[index - 1] == '\t' ||
+		n[index - 1] == '\n' ||
+		n[index - 1] == ',' ||
+		n[index - 1] == ';' ||
+		n[index - 1] == '.' ||
+		n[index - 1] == '!' ||
+		n[index - 1] == '?' ||
+		n[index - 1] == '"' ||
+		n[index - 1] == '(' ||
+		n[index - 1] == ')' ||
+		n[index - 1] == '{' ||
+		n[index - 1] == '}' ||
+		index == 0)
+			n[index] -= 32;
+		index++;
+	}
+	return (n);
+}
