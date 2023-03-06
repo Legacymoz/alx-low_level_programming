67de282e90dd5c5c7ecfa366ef3a5fc72d5cfb3c
Legacymoz <mozartkandie@gmail.com>
2023-03-06 12:49:12 -0800

    task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chess board
+ * @a: array
+ * Return: 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int n;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (n = 0; n < 0; n++)
+			_putchar(a[i][n]);
+		_putchar('\n');
+	}
+}
