d46b9784a764a305f75372908ee216189049b46c
Legacymoz <mozartkandie@gmail.com>
2023-03-06 13:07:11 -0800

    task 7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ * Return: 0
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int n;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (i = 0; i <= (size * size) - size; n = n + size - 1)
+	{
+		sum1 = sum1 + a[i];
+	}
+	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+	{
+		sum2 = sum2 + a[n];
+		printf("%d, %d\n", sum1, sum2);
+	}
+}
