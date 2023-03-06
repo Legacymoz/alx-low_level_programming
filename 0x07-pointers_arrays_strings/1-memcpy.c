fb8bf8f1749b5e4e727a3fb282446864b0535804
Legacymoz <mozartkandie@gmail.com>
2023-03-06 07:45:22 -0800

    task1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: pointer
+ * @src: pointer
+ * @n: number of times
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+	int a = n;
+
+	for (i = 0; i < a; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
