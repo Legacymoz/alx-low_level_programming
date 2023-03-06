b8a6f32e5398ac7bbf2a55f9932f34d471ceb92c
Legacymoz <mozartkandie@gmail.com>
2023-03-06 07:26:08 -0800

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with constant byte
+ * @s: the pointer
+ * @b: byte
+ * @n: the number of times
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
+
