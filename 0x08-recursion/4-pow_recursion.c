7f367bea31ffc136e959233eba0b78d9d7e5d23a
Legacymoz <mozartkandie@gmail.com>
2023-03-08 11:33:37 -0800

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - entry point
+ * @x: interger
+ * @y: power
+ * Return: 0 or 1
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
