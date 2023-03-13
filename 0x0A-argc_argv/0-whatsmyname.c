b96eff1339fd89f18cc4e1abf0f6cfa868fbc4d3
Legacymoz <mozartkandie@gmail.com>
2023-03-13 12:59:58 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: the arguments
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
